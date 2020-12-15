7c685892509e22d568f9e1421e19418d69bcaec5
Luis Navarro <lanavarrogs@gmail.com>
2020-12-15 11:56:32 -0600

    Se realizo los pagos con paypal

--- planes.php
+++ planes.php
@@ -93,9 +93,24 @@
         <script>paypal_sdk.Buttons({
             style: {
                 color: 'black',
-                size: 'responsive'
+                size: 'responsive',
+            },
+            createOrder: function(data,actions){
+                return actions.order.create({
+                    purchase_units: [{
+                        amount: {
+                            value: '0.01'
+                        }
+                    }]
+                });
+            },
+            onApprove: function(data, actions) {
+            return actions.order.capture().then(function(details) {
+                alert('Transaction completed by ' + details.payer.name.given_name);
+            });
             }
-        }).render('#paypal');</script>
+        }).render('#paypal');
+        </script>
         <?php include 'includes/templates/footer.php' ?>
     </body>
 </html>
