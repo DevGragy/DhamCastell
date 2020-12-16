fd41d6ac074fd97f5ad664555a5a2f651b1d6470
Luis Navarro <lanavarrogs@gmail.com>
2020-12-16 11:19:42 -0600

    Se agregaron los javascript

---
+++ includes/models/email.php
@@ -0,0 +1,14 @@
+<?php
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $asunto = $_POST['asunto'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+    
+    function enviarCorreo(){
+        
+
+        
+    }
+?>
+
---
+++ js/contacto.js
@@ -0,0 +1,40 @@
+//Variables
+const btnSubmit = document.querySelector('#contact_form');
+
+
+EventListeners();
+//Listeners
+function EventListeners(){
+    //Boton submit de contacto
+    btnSubmit.addEventListener('submit',enviarCorreo);
+}
+
+
+function enviarCorreo(e){
+    e.preventDefault();
+    console.log('Adentro');
+    let nombre = document.querySelector('#nombre').value,
+        apellido = document.querySelector('#apellido').value,
+        asunto = document.querySelector('#asunto').value,
+        correo = document.querySelector('#email').value,
+        mensaje = document.querySelector('#mensaje').value;
+
+    let datos = new FormData();
+    datos.append('nombre',nombre);
+    datos.append('apellido',apellido);
+    datos.append('email',correo);
+    datos.append('asunto',asunto)
+    datos.append('mensaje',mensaje);
+
+    fetch('http://localhost/DhamCastell/includes/models/email.php',{
+        method: 'POST',
+        body: datos
+    }).then(response => {
+        if(response.ok){
+            console.log(response.text());
+        }else{
+            throw "Error en la llamada";
+        }
+    }).then(json => console.log(json))
+    .catch(error=> console.log(error));
+}
